d21b533b06fc43a9f1436ac96b0bb760dc073fef
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-09 15:27:32 +0100

    init

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-// Nom    :
-// Prénom :
-// Groupe :
+// Nom    : Dombard
+// Prénom : Baptiste
+// Groupe : 2285
 
 const eButStart = document.querySelector(".app__controls__start");
 const eButLap = document.querySelector(".app__controls__lap");
+let intervalID = 0;
 let milSecondes = 0;
-let secondes = 40;
-let minutes = 59;
+let secondes = 0;
+let minutes = 0;
+let iTour = 0;
 function prefixe(num){
-    if (num<10){
-        return "0" + num;
-    }else {
-        return num;
-    }
+    return `${num}`.padStart(2, '0');
 }
 
+
 eButStart.addEventListener("click", (event)=>{
-    [eButStart.dataset.alternate, eButStart.textContent] = [eButStart.textContent, eButStart.dataset.alternate];
-    let intervalID = setInterval(()=>{
-        milSecondes++
-        if (milSecondes === 100){
-            secondes++
-            milSecondes=0;
-        }else if (secondes === 60){
-            minutes++
-            secondes=0;
-        }else if (minutes === 60){
-            clearInterval(intervalID);
-        }
-        document.querySelector("time").textContent = `${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}`;
-    }, 1);
+    if (intervalID === 0){
+        [eButStart.dataset.alternate, eButStart.textContent] = [eButStart.textContent, eButStart.dataset.alternate];
+        intervalID = setInterval(()=>{
+            milSecondes++
+            if (milSecondes === 100){
+                secondes++
+                milSecondes=0;
+            }else if (secondes === 60){
+                minutes++
+                secondes=0;
+            }else if (minutes === 60){
+                clearInterval(intervalID);
+            }
+            document.querySelector(".app__timer").textContent = `${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}`;
+        }, 1);
+    }else {
+        clearInterval(intervalID);
+        intervalID = 0;
+        [eButStart.textContent, eButStart.dataset.alternate] = [eButStart.dataset.alternate, eButStart.textContent];
+    }
+});
+
+
+eButLap.addEventListener("click", (event)=>{
+    iTour++;
+    document.querySelector(".app__laps").insertAdjacentHTML("beforeend", `<li class="app__lap">
+        <span class="app__lap-count">Tour ${iTour}</span>
+        <time class="app__lap-value" datatype=\"XX:YY:ZZ\">${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}</time>
+        </li>`)
 });
