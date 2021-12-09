16ae06560dc33f324e9b6a2fbd9418dedcd7fb92
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-09 13:20:26 +0100

    init

--- index.js
+++ index.js
@@ -5,8 +5,15 @@
 const eButStart = document.querySelector(".app__controls__start");
 const eButLap = document.querySelector(".app__controls__lap");
 let milSecondes = 0;
-let secondes = 0;
-let minutes = 0;
+let secondes = 40;
+let minutes = 59;
+function prefixe(num){
+    if (num<10){
+        return "0" + num;
+    }else {
+        return num;
+    }
+}
 
 eButStart.addEventListener("click", (event)=>{
     [eButStart.dataset.alternate, eButStart.textContent] = [eButStart.textContent, eButStart.dataset.alternate];
@@ -17,7 +24,10 @@ eButStart.addEventListener("click", (event)=>{
             milSecondes=0;
         }else if (secondes === 60){
             minutes++
+            secondes=0;
+        }else if (minutes === 60){
+            clearInterval(intervalID);
         }
-        console.log(milSecondes, secondes, minutes)
+        document.querySelector("time").textContent = `${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}`;
     }, 1);
 });
