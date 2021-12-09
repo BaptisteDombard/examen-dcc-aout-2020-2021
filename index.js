2f1643bcdfc396bd49ae3eefb27c2e50776ba929
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-09 12:28:56 +0100

    init

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 // Nom    :
 // Prénom :
 // Groupe :
+
+const eButStart = document.querySelector(".app__controls__start");
+const eButLap = document.querySelector(".app__controls__lap");
+let milSecondes = 0;
+let secondes = 0;
+let minutes = 0;
+
+eButStart.addEventListener("click", (event)=>{
+    [eButStart.dataset.alternate, eButStart.textContent] = [eButStart.textContent, eButStart.dataset.alternate];
+    let intervalID = setInterval(()=>{
+        milSecondes++
+        if (milSecondes === 100){
+            secondes++
+            milSecondes=0;
+        }else if (secondes === 60){
+            minutes++
+        }
+        console.log(milSecondes, secondes, minutes)
+    }, 1);
+});
