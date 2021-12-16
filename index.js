9c69bf5bb607a31a48ead580185d11a7d5454163
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-16 08:46:19 +0100

    init

--- index.js
+++ index.js
@@ -1,54 +1,67 @@
-// Nom    : Dombard
-// Prénom : Baptiste
-// Groupe : 2285
 
-const eButStart = document.querySelector(".app__controls__start");
-const eButLap = document.querySelector(".app__controls__lap");
-let intervalID = 0;
-let milSecondes = 0;
-let secondes = 0;
-let minutes = 0;
-let iTour = 0;
-function prefixe(num){
-    return `${num}`.padStart(2, '0');
-}
-
-
-eButStart.addEventListener("click", (event)=>{
-    if (intervalID === 0){
-        [eButStart.dataset.alternate, eButStart.textContent] = [eButStart.textContent, eButStart.dataset.alternate];
-        intervalID = setInterval(()=>{
-            milSecondes++
-            if (milSecondes === 100){
-                secondes++
-                milSecondes=0;
-            }else if (secondes === 60){
-                minutes++
-                secondes=0;
-            }else if (minutes === 60){
-                clearInterval(intervalID);
+(function(){
+    const oTimer = {
+        init(){
+            const eButStart = document.querySelector(".app__controls__start");
+            const eButLap = document.querySelector(".app__controls__lap");
+            let intervalID = 0;
+            let milSecondes = 0;
+            let secondes = 0;
+            let minutes = 0;
+            let iTour = 0;
+            function prefixe(num){
+                return `${num}`.padStart(2, '0');
             }
-            document.querySelector(".app__timer").textContent = `${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}`;
-        }, 1);
-        if (eButStart.textContent!=="Démarrer" && eButLap.textContent==="Tour"){
-            [eButLap.dataset.alternate, eButLap.textContent] = [eButLap.textContent, eButLap.dataset.alternate]
-        }
-    }else {
-        clearInterval(intervalID);
-        intervalID = 0;
-        [eButStart.textContent, eButStart.dataset.alternate] = [eButStart.dataset.alternate, eButStart.textContent];
-    }
-});
 
 
-eButLap.addEventListener("click", (event)=>{
-    iTour++;
-    document.querySelector(".app__laps").insertAdjacentHTML("beforeend", `<li class="app__lap">
+            eButStart.addEventListener("click", (event)=>{
+                if (intervalID === 0){
+                    [eButStart.dataset.alternate, eButStart.textContent] = [eButStart.textContent, eButStart.dataset.alternate];
+                    intervalID = setInterval(()=>{
+                        milSecondes++
+                        if (milSecondes === 100){
+                            secondes++
+                            milSecondes=0;
+                        }else if (secondes === 60){
+                            minutes++
+                            secondes=0;
+                        }else if (minutes === 60){
+                            clearInterval(intervalID);
+                        }
+                        document.querySelector(".app__timer").textContent = `${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}`;
+                    }, 1);
+                }else {
+                    clearInterval(intervalID);
+                    intervalID = 0;
+                    [eButStart.textContent, eButStart.dataset.alternate] = [eButStart.dataset.alternate, eButStart.textContent];
+                    [eButLap.textContent, eButLap.dataset.alternate] = [eButLap.dataset.alternate, eButLap.textContent];
+                }
+            });
+
+
+            eButLap.addEventListener("click", (event)=>{
+                if (eButLap.textContent === "Tour"){
+                    iTour++;
+                    document.querySelector(".app__laps").insertAdjacentHTML("beforeend", `<li class="app__lap">
         <span class="app__lap-count">Tour ${iTour}</span>
         <time class="app__lap-value" datatype=\"XX:YY:ZZ\">${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}</time>
         </li>`)
-    if (eButLap.textContent === "Effacer"){
-        clearInterval(intervalID);
-        // document.querySelector(".app__laps").
+                }
+                if (eButLap.textContent === "Effacer"){
+                    document.querySelector(".app__laps").innerHTML = "";
+                    milSecondes=0;
+                    secondes=0;
+                    minutes=0;
+                    document.querySelector(".app__timer").textContent = `${prefixe(minutes)}:${prefixe(secondes)}:${prefixe(milSecondes)}`;
+                    [eButLap.textContent, eButLap.dataset.alternate] = [eButLap.dataset.alternate, eButLap.textContent];
+                }
+            });
+        },
     }
-});
+    oTimer.init();
+})();
+
+
+
+
+
